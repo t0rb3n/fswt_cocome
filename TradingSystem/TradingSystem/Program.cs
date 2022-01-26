2beca2a917086226a57a44316fc8a3a64268143a
Max <compag1534@outlook.de>
2022-01-26 05:01:37 +0100

    first interface tests from ApplicationStore

--- TradingSystem/TradingSystem/Program.cs
+++ TradingSystem/TradingSystem/Program.cs
@@ -1,122 +1,69 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using TradingSystem.inventory.application.store;
 using TradingSystem.inventory.data;
 using TradingSystem.inventory.data.enterprise;
 using TradingSystem.inventory.data.store;
 
-namespace TradingSystem
+namespace TradingSystem;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        StoreApplication sApp = new(2);
+        
+        var store = sApp.GetStore();
+        Console.WriteLine(store);
+
+        System.Environment.Exit(0);
+
+        var item01 = sApp.GetProductStockItem(10000033);
+        Console.WriteLine(item01);
+
+        /*item01.SalesPrice += 2;
+        sApp.ChangePrice(item01);
+        
+        var item02 = sApp.GetProductStockItem(10000033);
+        Console.WriteLine(item02);*/
+
+        var product = sApp.GetAllProductSuppliers();
+        foreach (var p in product)
         {
-
-            IStoreQuery query = IDataFactory.GetInstance().GetStoreQuery();
-            
-            Console.WriteLine("QueryStoreById");
-            try
-            {
-                var store = query.QueryStoreById(1);
-                Console.WriteLine(store);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            
-            /*Console.WriteLine("\nQueryProducts");
-            try
-            {
-                var products = query.QueryProducts(1);
-                foreach (var p in products)
-                {
-                    Console.WriteLine($"{p.Id}, {p.Name}");
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }*/
-            
-            /*Console.WriteLine("\nQueryAllStockItems");
-            try
-            {
-                var stockItems = query.QueryAllStockItems(1);
-                foreach (var st in stockItems)
-                {
-                    Console.WriteLine($"{st.Amount}, {st.SalesPrice}, {st.Product.Name}, {st.Product.Barcode}");
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }*/
-            
-            Console.WriteLine("\nQueryStockItem");
-            try
-            {
-                var st = query.QueryStockItem(1, 10000125);
-                Console.WriteLine($"{st.Amount}, {st.SalesPrice}€, {st.Product.Name}, {st.Product.Barcode}");
-                
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            
-            Console.WriteLine("\nQueryLowStockItems");
-            try
-            {
-                var stockItems = query.QueryLowStockItems(1);
-                foreach (var st in stockItems)
-                {
-                    Console.WriteLine($"amount: {st.Amount}, minStock: {st.MinStock}, {st.SalesPrice}€, {st.Product.Name}");
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
-            /*using (var db = new DataContext())
-            {
-                var sql = db.Database.GenerateCreateScript();
-                Console.Write(sql);
-                System.Environment.Exit(0);
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-                
-                Enterprise enterprise = new();
-                enterprise.Id = 10001;
-                enterprise.Name = "AldiGroup";
-
-                Store store = new ()
-                {
-                    Id = 20001,
-                    Name = "Aldi",
-                    Location = "Wiesbaden"
-                };
-                
-                enterprise.Stores.Add(store);
-
-                db.Add(enterprise);
-                db.Add(store);
-                db.SaveChanges();
-
-                var store1 = db.Stores;
-                foreach (var s in store1)
-                {
-                    Console.Write(s);
-                }
-
-                Console.Write("\n");
-                
-                var en1 = db.Enterprises;
-                foreach (var e in en1)
-                {
-                    Console.Write(e);
-                }
-            }*/
+            Console.WriteLine(p);
         }
+        
+        Console.WriteLine("ProductOrder\n");
+        
+        ProductOrder po = new ProductOrder();
+        po.Store = store;
+        po.OrderingDate = DateTime.UtcNow;
+
+        OrderEntry oe1 = new OrderEntry();
+        oe1.Amount = 2;
+        oe1.ProductOrder = po;
+        oe1.Product = product[0];
+        
+        OrderEntry oe2 = new OrderEntry();
+        oe2.Amount = 3;
+        oe2.ProductOrder = po;
+        oe2.Product = product[1];
+        
+        po.OrderEntries.Add(oe1);
+        po.OrderEntries.Add(oe2);
+        
+        //sApp.OrderProducts(po);
+
+        var proOrder = sApp.GetProductOrder(7);
+        Console.WriteLine(proOrder);
+        
+        //sApp.RollInReceivedProductOrder(7);
+
+        var lowstock = sApp.GetProductsLowStockItems();
+        foreach (var item in lowstock)
+        {
+            Console.WriteLine(item);
+        }
+
     }
 }
