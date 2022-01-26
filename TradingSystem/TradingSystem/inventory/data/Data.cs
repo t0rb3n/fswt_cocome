2122b9e962615908a1cda2d69399f78a92ac9f43
Max <compag1534@outlook.de>
2022-01-26 04:45:19 +0100

    DependenceInjection in Data removed

--- TradingSystem/TradingSystem/inventory/data/Data.cs
+++ TradingSystem/TradingSystem/inventory/data/Data.cs
@@ -5,19 +5,13 @@ namespace TradingSystem.inventory.data;
 
 public class Data : IData
 {
-    private readonly DataContext _db;
-
-    public Data(DataContext db)
-    {
-        _db = db;
-    }
     public IEnterpriseQuery GetEnterpriseQuery()
     {
-        return new EnterpriceQuery(_db);
+        return new EnterpriseQuery();
     }
 
     public IStoreQuery GetStoreQuery()
     {
-        return new StoreQuery(_db);
+        return new StoreQuery();
     }
 }
