21bbda44b7b6133364c45e0a7f953782cc80c511
Max <compag1534@outlook.de>
2022-01-26 04:41:35 +0100

    unused classes removed

--- TradingSystem/TradingSystem/inventory/application/store/ProductStockItem.cs
+++
@@ -1,9 +0,0 @@
-using TradingSystem.inventory.data.enterprise;
-using TradingSystem.inventory.data.store;
-
-namespace TradingSystem.inventory.application.store;
-
-public class ProductStockItem : Product
-{
-    protected StockItem StockItem { get; set; }
-}
