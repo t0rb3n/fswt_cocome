fad914e323e4fc1c58218891c22e24faeef09ee8
Torben <torben@herborn.me>
2022-02-03 22:20:33 +0100

    Cleanup project to better file structure and use ASPNET as base

---
+++ CashDesk/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Transactions;
+using Tecan.Sila2.Discovery;
+
+namespace CashDesk;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddGrpcClient<EnterpriseRpc.EnterpriseRpcClient>(options =>
+        {
+            options.Address = new Uri("https://localhost:5001");
+        });
+        
+        //services.AddHostedService()
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+    }
+}
