639b20695fae00528ee90472b176cb69a6b4efaf
Torben <torben@herborn.me>
2022-02-04 12:38:22 +0100

    Add NoSuchProductException

---
+++ CashDesk/Exceptions/NoSuchProductException.cs
@@ -0,0 +1,23 @@
+﻿namespace CashDesk.Exceptions;
+
+public class NoSuchProductException : Exception
+{
+    private long _barcode;
+
+    public NoSuchProductException()
+    {
+    }
+
+    public NoSuchProductException(string message) : base(message)
+    {
+    }
+
+    public NoSuchProductException(string message, Exception inner) : base(message, inner)
+    {
+    }
+
+    public NoSuchProductException(long barcode)
+    {
+        this._barcode = barcode;
+    }
+}
