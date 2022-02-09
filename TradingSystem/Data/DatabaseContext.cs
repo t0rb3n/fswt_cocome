b16f417567d2716e00bb4bd2f31408e896d7a99b
Max <compag1534@outlook.de>
2022-02-09 14:04:59 +0100

    add and commit local connection to Postgresql

--- TradingSystem/Data/DatabaseContext.cs
+++ TradingSystem/Data/DatabaseContext.cs
@@ -18,6 +18,7 @@ public class DatabaseContext : DbContext
     {
         optionsBuilder
             .UseNpgsql("host=ec2-54-155-194-191.eu-west-1.compute.amazonaws.com;database=d6v10jgjrtfjnt;username=mhxaavrkfwmegj;password=fc1cc9bdc3a621aa753d50896e87f00d2420354242cbd92b20331bf6cc1e16a4")
+            //.UseNpgsql("host=localhost;database=tradingsystem;username=dummy;password=dummy123")
             .UseSnakeCaseNamingConvention();
     }
 }
