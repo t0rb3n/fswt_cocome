343735c8c6176725b35c67e74d266c80fdb13e0f
Max <compag1534@outlook.de>
2022-02-12 22:45:25 +0100

    new queries added to the IEnterpriseQuery interface

--- TradingSystem/Data/Enterprise/IEnterpriseQuery.cs
+++ TradingSystem/Data/Enterprise/IEnterpriseQuery.cs
@@ -3,5 +3,7 @@ namespace Data.Enterprise;
 public interface IEnterpriseQuery
 {
     public Enterprise QueryEnterpriseById(long enterpriseId, DatabaseContext dbc);
+    public IList<Store.Store> QueryStores(long enterpriseId, DatabaseContext dbc);
+    public IList<ProductSupplier> QueryProductSuppliers(long enterpriseId, DatabaseContext dbc);
     public long QueryMeanTimeToDelivery(ProductSupplier productSupplier, Enterprise enterprise, DatabaseContext dbc);
 }
