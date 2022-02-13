248a2dcac479163f05d44c424759bfbcd1864095
Max <compag1534@outlook.de>
2022-02-13 12:24:17 +0100

    Updated the startup process of the web servers

--- TradingSystem/WebServerEnterprise/StartUpEnterprise.cs
+++ TradingSystem/WebServerEnterprise/StartUpEnterprise.cs
@@ -1,4 +1,5 @@
 using Application.Enterprise;
+using Data.Exceptions;
 using WebServerEnterprise.Services;
 
 namespace WebServerEnterprise;
@@ -12,11 +13,24 @@ public class StartUpEnterprise
         {
             Console.WriteLine($"\t{arg}");
         }
+
+        var enterpriseId = Convert.ToInt64(args[0]);
         
         var builder = WebApplication.CreateBuilder(args); 
-        var application = new EnterpriseApplication(Convert.ToInt64(args[0]));
+        var application = new EnterpriseApplication(enterpriseId);
+
+        var enterprise = new EnterpriseDTO();
         
-        var enterprise = application.GetEnterprise();
+        try
+        {
+            enterprise = application.GetEnterprise();
+        }
+        catch (DatabaseNotAvailableException e)
+        {
+            Console.WriteLine(e);
+            Environment.Exit(0);
+        }
+        
         builder.Configuration["ServerInfo:EnterpriseName"] = enterprise.EnterpriseName;
         
         // Add services to the container.
@@ -29,26 +43,28 @@ public class StartUpEnterprise
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            // The default HSTS value is 30 days.
             app.UseHsts();
         }
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseRouting();
-
-
+        
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller}/{action=Index}/{id?}");
 
         app.MapFallbackToFile("index.html");
+        
+        // Add service for enterprise grpc server
         app.MapGrpcService<EnterpriseGrpcService>();
         app.MapGet(
             "/grpc",
             () => "Communication with gRPC endpoints must be made through a gRPC client."
         );
-
+        
+        // start asp.net enterprise web server
         app.Run();
     }
 }
