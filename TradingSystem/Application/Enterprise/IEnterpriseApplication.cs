26346790d198c56a36ac1da25a9507aea52dc8dd
Max <compag1534@outlook.de>
2022-02-13 11:46:29 +0100

    List to IList and ChangePrice changed to void

--- TradingSystem/Application/Enterprise/IEnterpriseApplication.cs
+++ TradingSystem/Application/Enterprise/IEnterpriseApplication.cs
@@ -5,17 +5,19 @@ namespace Application.Enterprise;
 public interface IEnterpriseApplication
 {
     public EnterpriseDTO GetEnterprise();
+    public IList<StoreDTO> GetEnterpriseStores();
+    public IList<ProductSupplierDTO> GetEnterpriseProductSupplier();
     public ReportDTO GetStockReport(Data.Store.Store store);
     public ReportDTO GetStockReport(Data.Enterprise.Enterprise enterprise);
     public ReportDTO GetMeanTimeToDeliveryReport(Data.Enterprise.Enterprise enterprise);
     public StoreEnterpriseDTO GetStore(long storeId);
-    public List<ProductStockItemDTO> GetProductsLowStockItems(long storeId);
-    public List<ProductSupplierDTO> GetAllProductSuppliers(long storeId);
-    public List<ProductSupplierStockItemDTO> GetAllProductSupplierStockItems(long storeId);
+    public IList<ProductStockItemDTO> GetProductsLowStockItems(long storeId);
+    public IList<ProductSupplierDTO> GetAllProductSuppliers(long storeId);
+    public IList<ProductSupplierStockItemDTO> GetAllProductSupplierStockItems(long storeId);
     public void OrderProducts(ProductOrderDTO productOrder, long storeId);
     public ProductOrderDTO GetProductOrder(long productOrderId);
     public void RollInReceivedProductOrder(ProductOrderDTO productOrder, long storeId);
-    public bool ChangePrice(long stockItemId, double newPrice);
+    public void ChangePrice(long stockItemId, double newPrice);
     public void MakeBookSale(SaleDTO saleDto);
     public ProductStockItemDTO GetProductStockItem(long productBarcode, long storeId);
     
