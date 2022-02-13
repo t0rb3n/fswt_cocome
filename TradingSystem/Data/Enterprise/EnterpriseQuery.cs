b83a9ce6bc912936081686fb1f1186003e2a92fe
Max <compag1534@outlook.de>
2022-02-13 08:50:25 +0100

    Implemented new interfaces and QueryMeanTimeToDelivery()

--- TradingSystem/Data/Enterprise/EnterpriseQuery.cs
+++ TradingSystem/Data/Enterprise/EnterpriseQuery.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Data.Enterprise;
 
 public class EnterpriseQuery : IEnterpriseQuery
@@ -11,8 +13,49 @@ public class EnterpriseQuery : IEnterpriseQuery
         return result;
     }
 
+    public IList<Store.Store> QueryStores(long enterpriseId, DatabaseContext dbc)
+    {
+        var result = dbc
+                .Stores
+                .Where(store => store.Enterprise.Id == enterpriseId)
+                .ToList() 
+                     ?? throw new Exception($"Can't find stores from enterprise id {enterpriseId}");
+        return result;
+    }
+
+    public IList<ProductSupplier> QueryProductSuppliers(long enterpriseId, DatabaseContext dbc)
+    {
+        var result = dbc
+                         .Enterprises
+                         .Find(enterpriseId)
+                         
+                     ?? throw new Exception($"Can't find product supplier from enterprise id {enterpriseId}");
+
+        dbc.Entry(result).Collection(enterprise => enterprise.ProductSuppliers).Load();
+        
+        return result.ProductSuppliers;
+    }
+    
     public long QueryMeanTimeToDelivery(ProductSupplier productSupplier, Enterprise enterprise, DatabaseContext dbc)
     {
-        throw new NotImplementedException();
+        var result = dbc
+            .ProductOrders
+            .Where(order => 
+                order.DeliveryDate != DateTime.MinValue 
+                && order.OrderEntries.Any(e => 
+                    e.Product.ProductSupplier.Id == productSupplier.Id)
+                && order.Store.Enterprise.Id == enterprise.Id)
+            .ToList();
+
+        long meanTime = 0;
+        
+        foreach (var order in result)
+        {
+            meanTime += order.DeliveryDate.Ticks - order.OrderingDate.Ticks;
+        }
+        if (meanTime != 0) meanTime /= result.Count;
+
+        return meanTime;
+
     }
 }
